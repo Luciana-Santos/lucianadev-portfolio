b49899f8f6edbfa90042fe934675f399a752e323
Luciana Santos <github@luciana.dev>
2024-01-29 20:17:43 -0300

    Initiating project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ function App() {
   return (
     <>
       <h1>Work in progress</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        veritatis ab quibusdam.
+      </p>
     </>
   )
 }
