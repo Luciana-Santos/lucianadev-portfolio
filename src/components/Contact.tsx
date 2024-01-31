3a120dd12aa2de3482f13f10e0f15c7dc23dcf20
Luciana Santos <95719499+Luciana-Santos@users.noreply.github.com>
2024-01-31 16:33:26 -0300

    feat: 🎸 Contact section finished (#13)

---
+++ src/components/Contact.tsx
@@ -0,0 +1,37 @@
+import { IoMailOutline } from 'react-icons/io5'
+import Title from '../ui/Title'
+
+export default function Contact() {
+  return (
+    <section className="relative section-layout">
+      <Title>Contato</Title>
+
+      <div className="flex items-center  flex-col-reverse md:flex-row justify-between gap-14">
+        <div>
+          <img
+            src="https://github.com/Luciana-Santos/Lucianadss-portfolio/blob/main/src/assets/img/contato-img.png?raw=true"
+            alt=""
+          />
+        </div>
+        <div>
+          <p className="mb-10">
+            Disponível para novos projetos.
+            <br />
+            Entre em contato e bora bater um papo!
+          </p>
+          <ul>
+            <li>
+              <a
+                href="mailto:contato@luciana.dev"
+                className="flex gap-2.5 items-center"
+              >
+                <IoMailOutline size={40} className="text-mdGreen" />
+                <span className="link">contato@luciana.dev</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
