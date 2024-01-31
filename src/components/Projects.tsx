9819cdd0393ee78cb6d56eb110b972dca8aea922
Luciana Santos <95719499+Luciana-Santos@users.noreply.github.com>
2024-01-31 17:08:40 -0300

    feat: 🎸 Projects parallax finished (#14)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,13 +4,18 @@ import ProjectItem from './ProjectItem'
 
 export default function Projects() {
   return (
-    <section className="section-layout">
-      <Title>Projetos</Title>
-      <div className="flex flex-wrap justify-center gap-7">
-        {projects.map((project) => {
-          return <ProjectItem key={project.id} data={project} />
-        })}
+    <section className="planet-bottom relative">
+      <div className="section-layout">
+        <Title>Projetos</Title>
+        <div className="flex flex-wrap justify-center gap-7">
+          {projects.map((project) => {
+            return <ProjectItem key={project.id} data={project} />
+          })}
+        </div>
       </div>
+
+      <div className="w-full h-[247px] absolute left-0 z-[-200] bottom-[-166px] back-cloud-contact"></div>
+      <div className="w-full h-[385px] absolute left-0 z-[-100] front-cloud-contact bottom-[-185px]"></div>
     </section>
   )
 }
