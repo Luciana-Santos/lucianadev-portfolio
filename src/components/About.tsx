220a79bccf15e1d0e5e36d0a23123b2ec17482a8
Luciana Santos <github@luciana.dev>
2024-01-31 17:58:00 -0300

    feat: 🎸 Reveal animation added
    Reveal animation as the page scrolls added

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 import Title from '../ui/Title'
 import Techs from './Techs'
 import { getYearsOld } from '../utils/currentAge'
+import { Fade } from 'react-awesome-reveal'
 
 export default function About() {
   const { scrollY } = useScroll()
@@ -21,50 +22,59 @@ export default function About() {
         ></motion.div>
 
         <div className="section-layout">
-          <Title>Sobre</Title>
+          <Fade direction="down">
+            <Title>Sobre</Title>
+          </Fade>
 
           <div className="grid gap-10 md:grid-cols-[2fr_1fr] items-center">
-            <div className="grid gap-5">
-              <p>
-                Sou Luciana Santos, {getYearsOld()} anos, residente de São
-                Paulo-SP e Desenvolvedora front end.
-              </p>
-              <p>
-                Minha parte favorita em trabalhar com desenvolvimento web é a
-                possibilidade de transformar ideias em linhas de códigos
-                executáveis e ver o resultado fluir na tela. Estou sempre
-                aprendendo algo novo e subindo mais um degrau para chegar no meu
-                objetivo, que não é estático e está em constante
-                desenvolvimento.
-              </p>
-              <div className="flex gap-10 mt-8">
-                <div>
-                  <h3 className="mb-3 text-2xl font-berkshire">Links:</h3>
-                  <ul>
-                    <li>
-                      <a href="#" className="link">
-                        Linkedin
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="link">
-                        GitHub
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-                  <h3 className="mb-3 text-2xl font-berkshire">Tecnologias:</h3>
-                  <ul className="flex gap-5 items-center flex-wrap">
-                    <Techs />
-                    <Techs />
-                  </ul>
+            <Fade direction="left">
+              <div className="grid gap-5">
+                <p>
+                  Sou Luciana Santos, {getYearsOld()} anos, residente de São
+                  Paulo-SP e Desenvolvedora front end.
+                </p>
+                <p>
+                  Minha parte favorita em trabalhar com desenvolvimento web é a
+                  possibilidade de transformar ideias em linhas de códigos
+                  executáveis e ver o resultado fluir na tela. Estou sempre
+                  aprendendo algo novo e subindo mais um degrau para chegar no
+                  meu objetivo, que não é estático e está em constante
+                  desenvolvimento.
+                </p>
+                <div className="flex gap-10 mt-8">
+                  <div>
+                    <h3 className="mb-3 text-2xl font-berkshire">Links:</h3>
+                    <ul>
+                      <li>
+                        <a href="#" className="link">
+                          Linkedin
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#" className="link">
+                          GitHub
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h3 className="mb-3 text-2xl font-berkshire">
+                      Tecnologias:
+                    </h3>
+                    <ul className="flex gap-5 items-center flex-wrap">
+                      <Techs />
+                      <Techs />
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mx-auto m-w-[400px]">
-              <img src="/assets/sobre-section.png" alt="sobre imagem" />
-            </div>
+            </Fade>
+
+            <Fade direction="right">
+              <div className="mx-auto m-w-[400px]">
+                <img src="/assets/sobre-section.png" alt="sobre imagem" />
+              </div>
+            </Fade>
           </div>
         </div>
       </section>
